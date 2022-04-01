be0fd18b63bda9e2ba722c3146745512ef4a8bdd
andrew <andrewoliveira282@gmail>
2022-04-01 12:40:59 -0300

    atualização do app aplicando o modal

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
 class Despesa{
     constructor(ano,dia,mes,tipo,descricao,valor) {
-        this.ano
-        this.dia
-        this.mes
-        this.tipo
-        this.descricao
-        this.valor
+        this.ano=ano
+        this.dia=dia
+        this.mes=mes
+        this.tipo=tipo
+        this.descricao=descricao
+        this.valor=valor
+
     }
+    validarDados(){
+        for(let i in this){
+            if (this[i]==undefined || this[i]=='' || this[i]==null){
+                return false
+            }
+            return true
+        }
 }
+}
+class Bd{
+    constructor() {
+        let id =localStorage.getItem('id')
+        if(id===null){
+            localStorage.setItem('id',0)
+        }
+    }
+    getProximoItem(){
+        let proximoId= localStorage.getItem('id')
+        return parseInt(proximoId)+1
+    }
+     gravar(d){
+
+         let id = this.getProximoItem()
+         localStorage.setItem(id,JSON.stringify(d))
+
+         localStorage.setItem('id',id)
+    }
+
+
+}
+let bd = new Bd();
 function cadastrarDispesas(){
    let ano = document.getElementById('ano').value
     let dia = document.getElementById('dia').value
@@ -16,7 +47,14 @@ function cadastrarDispesas(){
     let descricao=document.getElementById('descricao').value
     let valor=document.getElementById('valor').value
    let despesa = new Despesa(ano,dia,mes,tipo,descricao,valor)
-    console.log(despesa)
+    if(despesa.validarDados()){
+        console.log('dados validos')
+        $('sucessoGravacao').modal('show')
+    }else {
+        //console.log('dados invalidos ')
+        $('#erroGravacao').modal('show')
+    }
+    bd.gravar(despesa)
 
 
 
